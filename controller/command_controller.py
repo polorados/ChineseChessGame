f8d789a2a84e103046a637b257cf6c3c6cdecd48
허성학 <heosunghak@heoseonghag-ui-MacBookPro.local>
2025-11-02 00:34:04 +0800

    WIP: rename SaveGame -> save_game and fixed wrong codes

---
+++ controller/command_controller.py
@@ -0,0 +1,17 @@
+class CommandController:
+
+    def get_input(self):
+        command = input()
+        return command
+    def start_game(self):
+        command = get_input()
+        while (True):
+            if command[0] == "new":
+
+            if command[0] == "name":
+            if command[0] == "show":
+            
+            #move A1 A2
+            if command[0] == "move":
+            if command[0] == "save":
+            if command[0] == "load":
