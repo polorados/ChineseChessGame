d665441a9d3f83d8cd503b8dbe756f1afe038ffb
Your Name <your.email@example.com>
2025-11-23 20:03:53 +0800

    new directory for record and playback

--- model/save_game.py
+++ model/save_game.py
@@ -7,8 +7,13 @@ class SaveGame:
     @staticmethod
     def save_game(game, filename: str = "game.jungle"):
         
+        os.makedirs('data', exist_ok=True)
+        filepath = os.path.join('data', filename)
+
+
+        
         try:
-            with open(filename, 'wb') as file:
+            with open(filepath, 'wb') as file:
                 pickle.dump(game, file)
             
         except Exception as e:
@@ -17,8 +22,9 @@ class SaveGame:
     @staticmethod
     def load_game(filename: str = "game.jungle"):
         
+        filepath = os.path.join('data', filename)
         try:
-            with open(filename, 'rb') as file:
+            with open(filepath, 'rb') as file:
                 game = pickle.load(file)
             print(f"Game successfully loaded from '{filename}'")
             time.sleep(0.5)
@@ -32,13 +38,18 @@ class SaveGame:
     def get_jungle_save_files():
         """
         Returns a list of all .jungle save files in the current directory.
+
+        
         """
+        folder_path = os.path.join('data')
         jungle_files = []
         
+        os.makedirs(folder_path, exist_ok=True)
         # Get all files in current directory
-        for filename in os.listdir('.'):
+        for filename in os.listdir(folder_path):
+            full_path = os.path.join(folder_path, filename)
             # Check if file ends with .jungle and is a file (not directory)
-            if filename.endswith('.jungle') and os.path.isfile(filename):
+            if filename.endswith('.jungle') and os.path.isfile(full_path):
                 jungle_files.append(filename)
         
         return jungle_files
@@ -48,12 +59,14 @@ class SaveGame:
         """
         Returns a list of all .record save files in the current directory.
         """
+        folder_path = os.path.join('data')
         jungle_files = []
-        
+        os.makedirs(folder_path, exist_ok=True)
         # Get all files in current directory
-        for filename in os.listdir('.'):
+        for filename in os.listdir(folder_path):
+            full_path = os.path.join(folder_path, filename)
             # Check if file ends with .jungle and is a file (not directory)
-            if filename.endswith('.record') and os.path.isfile(filename):
+            if filename.endswith('.record') and os.path.isfile(full_path):
                 jungle_files.append(filename)
         
         return jungle_files
